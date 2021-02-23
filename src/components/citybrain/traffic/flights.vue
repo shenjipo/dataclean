9d9f5648aa822f0f24efe21ccc21f5b9326c2009
XINXINSQJ <37903196+XINXINSQJ@users.noreply.github.com>
2021-02-23 14:00:42 +0800

    flights接收指标

--- src/components/citybrain/traffic/flights.vue
+++ src/components/citybrain/traffic/flights.vue
@@ -1,6 +1,64 @@
 <template>
+<<<<<<< Updated upstream
+  <div>
+    <el-button type="primary" @click="query">清洗</el-button>
+    <el-form :inline="true" :model="result">
+      <el-form-item label="清洗记录条数">
+        <el-input v-model="result.total" placeholder="MB"></el-input>
+      </el-form-item>
+      <el-form-item label="清洗速度">
+        <el-input v-model="result.speed" placeholder="万条/s"></el-input>
+      </el-form-item>
+      <el-form-item label="清洗率">
+        <el-input v-model="result.ratio" placeholder="%"></el-input>
+      </el-form-item>
+      <el-form-item label="清洗时间">
+        <el-input v-model="result.time" placeholder="s"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-form inline="true" :v-model="flightstarget">
+      <el-form-item label="查准率">
+        <el-input v-model="flightstarget.p" placeholder=""></el-input>
+      </el-form-item>
+      <el-form-item label="召回率">
+        <el-input v-model="flightstarget.r" placeholder=""></el-input>
+      </el-form-item>
+      <el-form-item label="F1">
+        <el-input v-model="flightstarget.f" placeholder=""></el-input>
+      </el-form-item>
+    </el-form>
+
+    <!--卡片视图区域-->
+    <el-card>
+      <!--用户列表区域-->
+      <el-table :data="flightsDirtyList" border stripe>
+        <!--缩印列-->
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="mobile"></el-table-column>
+        <el-table-column label="角色" prop="role_name"></el-table-column>
+      </el-table>
+    </el-card>
+    <!--卡片视图区域-->
+    <el-card>
+      <!--用户列表区域-->
+      <el-table :data="flightsCleanList" border stripe>
+        <!--缩印列-->
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="mobile"></el-table-column>
+        <el-table-column label="角色" prop="role_name"></el-table-column>
+      </el-table>
+    </el-card>
+    <!--分页区域-->
+    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                   :current-page="queryInfo.pagenum" :page-sizes="[1, 2, 5, 10]" :page-size="queryInfo.pagesize"
+                   layout="total, sizes, prev, pager, next, jumper" :total="total">
+=======
     <div>
-        <el-button type="primary" @click="query"></el-button>
+        <el-button type="primary" @click="query">清洗</el-button>
       <el-form :inline="true" :model="result">
         <el-form-item label="清洗记录条数">
           <el-input v-model="result.total" placeholder="MB"></el-input>
@@ -8,11 +66,20 @@
         <el-form-item label="清洗速度">
           <el-input v-model="result.speed" placeholder="万条/s"></el-input>
         </el-form-item>
-        <el-form-item label="清洗率">
-          <el-input v-model="result.ratio" placeholder="%"></el-input>
-        </el-form-item>
         <el-form-item label="清洗时间">
           <el-input v-model="result.time" placeholder="s"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <el-form inline="true" :v-model="flightstarget">
+        <el-form-item label="查准率">
+          <el-input v-model="flightstarget.p" placeholder=""></el-input>
+        </el-form-item>
+        <el-form-item label="召回率">
+          <el-input v-model="flightstarget.r" placeholder=""></el-input>
+        </el-form-item>
+        <el-form-item label="F1">
+          <el-input v-model="flightstarget.f" placeholder=""></el-input>
         </el-form-item>
       </el-form>
 
@@ -44,18 +111,20 @@
       <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
                      :current-page="queryInfo.pagenum" :page-sizes="[1, 2, 5, 10]" :page-size="queryInfo.pagesize"
                      layout="total, sizes, prev, pager, next, jumper" :total="total">
+>>>>>>> Stashed changes
 
-      </el-pagination>
-    </div>
+    </el-pagination>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios'
-export default {
+  import axios from 'axios'
+  export default {
     data(){
       return{
         flightsCleanList:[],
         flightsDirtyList:[],
+        flightstarget:{},
         queryInfo: {
           query: '',
           //当前的页数
@@ -67,23 +136,34 @@ export default {
         result:{
           total:'10万条',
           speed:'2000条/s',
-          ratio:'80',
           time:'20s'
         }
       }
     },
     methods:{
-        async query(){
-            const res = await axios.get('detection_dirty_data',{},
+<<<<<<< Updated upstream
+      async query(){
+        const res = await axios.get('raha_detection',{params: this.flightstarget},
                 {'Access-Control-Allow-Origin':'*'});
-            console.log(res);
+        console.log(res)
+        this.flightstarget=res.data
+        console.log(this.flightstarget)
+      },
+=======
+        async query(){
+            const res = await axios.get('raha_detection',{params: this.flightstarget},
+                {'Access-Control-Allow-Origin':'*'});
+            console.log(res)
+            this.flightstarget=res.data
+            console.log(this.flightstarget)
         },
+>>>>>>> Stashed changes
     }
-}
+  }
 </script>
 
 <style lang="less" scoped>
-.el-card{
-  height: 350px;
-}
+  .el-card{
+    height: 350px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://127.0.0.1:8002/dataclean/'
+axios.defaults.baseURL = 'http://10.0.105.87:8003/'
 new Vue({
   router,
   render: h => h(App)
