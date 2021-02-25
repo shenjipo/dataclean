3c097879c1edb08dd4f3f654031d37709450f5fe
XINXINSQJ <37903196+XINXINSQJ@users.noreply.github.com>
2021-02-25 16:20:54 +0800

    增加分页

--- src/components/citybrain/traffic/filightData1.vue
+++ src/components/citybrain/traffic/filightData1.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <el-card>
-            <el-table  :data="DataList"  border stripe max-height="650">
+            <el-table  :data="DataList.slice((currentPage-1)*pagesize,currentPage*pagesize)"  border stripe max-height="650">
                 <el-table-column label="tuple_id" property="tuple_id"></el-table-column>
                 <el-table-column label="src" property="src"></el-table-column>
                 <el-table-column label="flight" property="flight"></el-table-column>
@@ -11,6 +11,14 @@
                 <el-table-column label="act_arr_time" property="act_arr_time"></el-table-column>
                 <el-table-column label="是否正确" property=""></el-table-column>
             </el-table>
+            <el-pagination small layout="total, prev, pager, next,jumper"
+                           :total="DataList.length"
+                           :page-size="pagesize"
+                           pager-count="10"
+                           :current-page="currentPage"
+                           @current-change="handleCurrentChange"
+            >
+            </el-pagination>
             <el-row class="btns">
                 <el-button type="primary">检测</el-button>
                 <el-button type="success">修复</el-button>
@@ -26,6 +34,8 @@ import axios from 'axios'
         data(){
             return {
                 DataList:[],
+                pagesize:10,
+                currentPage:1,
             }
         },
         created() {
@@ -36,7 +46,9 @@ import axios from 'axios'
             })
         },
         methods:{
-
+            handleCurrentChange(val){
+                this.currentPage=val;
+            }
         }
     }
 </script>
