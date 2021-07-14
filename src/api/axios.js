49268893d6b8cc86b824ac91bebb80bb45c57d4b
1532281546@qq.com <1532281546@qq.com>
2021-07-14 15:14:10 +0800

    wang

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,64 +1,89 @@
 import axios from 'axios';
+import {Loading} from 'element-ui'
+
 axios.defaults.baseURL = 'http://10.11.24.118:8181/dataclean/'
 axios.defaults.crossDomain = true;
 //axios.defaults.withCredentials = true;
 axios.defaults.timeout = 30000;
-axios.$get = function(url, params,myConfig){
-    return new Promise((resolve, reject) =>{
-        let config= {
-            params: params
-        };
-        if(myConfig){
-            config=Object.assign(config,myConfig);
-        }
-        axios.get(url,config,
-            {
-                headers:{'Content-Type':'application/x-www-form-urlencoded'},
-            }).then(res => {
-            resolve(res.data);
-        }).catch(err =>{
-            reject(err)
-        })
-    })
-}
-axios.$post = function(url, params) {
-    var headers = {
-        'Content-Type':'application/json;charset=utf-8;',
+
+//请求拦截
+axios.interceptors.request.use(config => {
+  var loading = Loading.service({
+    lock: false,
+    text: 'Loading',
+    fullscreen: false,
+    spinner: 'el-icon-loading',
+    background: 'rgba(0, 0, 0, 0.7)'
+  });
+
+});
+//响应拦截
+axios.interceptors.response.use(config => {
+  var loading = Loading.service({
+    lock: false,
+    text: 'Loading',
+    spinner: 'el-icon-loading',
+    background: 'rgba(0, 0, 0, 0.7)'
+  });
+  loading.close();
+  return config
+});
+axios.$get = function (url, params, myConfig) {
+  return new Promise((resolve, reject) => {
+    let config = {
+      params: params
     };
-    return new Promise((resolve, reject) => {
-        axios.post(url, JSON.stringify(params),
-            {
-                headers:headers,
-            }).then(res => {
-            resolve(res.data);
-        })
-            .catch(err =>{
-                console.log(err)
-                reject(err.data)
-            })
+    if (myConfig) {
+      config = Object.assign(config, myConfig);
+    }
+    axios.get(url, config,
+        {
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        }).then(res => {
+      resolve(res.data);
+    }).catch(err => {
+      reject(err)
     })
+  })
 }
-axios.$put = function(url, params) {
-    return new Promise((resolve, reject) => {
-        axios.put(url, JSON.stringify(params),
-            {
-                headers:{'Content-Type':'application/x-www-form-urlencoded'},
-            },
-        ).then(res => {
-            resolve(res.data);
-        })
-            .catch(err =>{
-                reject(err.data)
-            })
+axios.$post = function (url, params) {
+  var headers = {
+    'Content-Type': 'application/json;charset=utf-8;',
+  };
+  return new Promise((resolve, reject) => {
+    axios.post(url, JSON.stringify(params),
+        {
+          headers: headers,
+        }).then(res => {
+      resolve(res.data);
     })
+        .catch(err => {
+          console.log(err)
+          reject(err.data)
+        })
+  })
 }
-axios.$delete = (url) =>{
-    return new Promise((resolve, reject) =>{
-        axios.delete(url).then(res => {
-            resolve(res.data);
-        }).catch(err =>{
-            reject(err.data)
-        })
+axios.$put = function (url, params) {
+  return new Promise((resolve, reject) => {
+    axios.put(url, JSON.stringify(params),
+        {
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        },
+    ).then(res => {
+      resolve(res.data);
     })
+        .catch(err => {
+          reject(err.data)
+        })
+  })
+}
+axios.$delete = (url) => {
+  return new Promise((resolve, reject) => {
+    axios.delete(url).then(res => {
+      resolve(res.data);
+    }).catch(err => {
+      reject(err.data)
+    })
+  })
 }
 export default axios
